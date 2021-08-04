7d8022a8ecec862468cbdcda265b8508ebff060f
rafael-tivane <luislangabusiness@gmail.com>
2021-08-04 12:00:33 +0100

    retifying built in classes name on footer tag

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,11 +6,11 @@ import './styles.css';
 const Footer = () => {
     return (
         <Container as="footer" fluid className="footer my-5" >
-            <Container fluid className="align-center d-flex flex-md-row flex-column py-5 footer__division">
+            <Container fluid className="align-items-center d-flex flex-md-row flex-column py-5 footer__division">
                 <Link to="" className="footer__logo-container">
                     <Image src={logo}  className="footer__logo d-block w-100 h-100" alt="Logo" />
                 </Link>
-                <ul className="align-center mt-5 d-flex flex-md-row flex-column w-100 justify-content-between footer__list">
+                <ul className="align-items-center mt-5 d-flex flex-md-row flex-column w-100 justify-content-between footer__list">
                     <li className="footer__item mb-3">
                         <Link to="/" className="text-uppercase text-white text-decoration-none opacity-8 footer__link">Home</Link>
                     </li>
@@ -21,7 +21,7 @@ const Footer = () => {
                         <Link to="/plan" className="text-uppercase text-white text-decoration-none opacity-8 footer__link">Create your plan</Link>
                     </li>
                 </ul>
-                <div className="align-center justify-center d-flex mt-5 justify-content-md-end flex-md-grow-1 footer__social-media-container">
+                <div className="align-items-center justify-center d-flex mt-5 justify-content-md-end flex-md-grow-1 footer__social-media-container">
                     <Link to="/" className="me-4 footer__social-media">
                         <svg className=" w-100 h-100" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="#FEFCF7" d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z"/></svg>
                     </Link>
